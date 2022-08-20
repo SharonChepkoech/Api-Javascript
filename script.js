1fed83480d87a36018d60b17aa141f80d1ed6820
Wanjiru-Kamau <njirukmaureen@gmail.com>
2022-08-20 08:49:39 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let fetch = require("node-fetch")
+fetch('https://jsonplaceholder.typicode.com/todos',{
+    method: 'POST',
+    headers:{
+        "Content-Type": "application/json"
+    },
+    body:JSON.stringify({
+        userId: 70,
+        title: "Cheko",
+        completed: true
+    })
+})
+.then(function (response) {
+return response.json();
+})
+.then(function (data) {
+console.log('success', data);
+})
+.catch(function (error) {
+console.log('error', error);
+})
